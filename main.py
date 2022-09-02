b2dd0e3ac766ba6a1b1e25077fcbe789e53f7c05
Yusufkhan-1443158 <67864867+Yusufkhan-1443158@users.noreply.github.com>
2022-09-02 19:27:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,11 +13,7 @@ def test_bucketing_by_number_of_cycles():
   print("Counting batteries by usage cycles...\n")
   counts = count_batteries_by_usage([100, 300, 500, 600, 900, 1000])
   print(counts)
-  assert(counts["lowCount"] == 2)
-  assert(counts["mediumCount"] == 3)
-  assert(counts["highCount"] == 1)
   print("Done counting :)")
 
-
-if __name__ == '__main__':
+  if __name__ == '__main__':
   test_bucketing_by_number_of_cycles()
