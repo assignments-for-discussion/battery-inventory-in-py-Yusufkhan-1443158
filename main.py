21820bb966f467da1cb2075a7c2699f49e8b0180
Yusufkhan-1443158 <67864867+Yusufkhan-1443158@users.noreply.github.com>
2022-09-02 19:43:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,5 +15,5 @@ def test_bucketing_by_number_of_cycles():
   print(counts)
   print("Done counting :)")
 
-if __name__ == '__main__':
+if __name__ == '_main_':
   test_bucketing_by_number_of_cycles()
